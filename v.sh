e540a139cd7e5333b6ac69f12ba494115d8cbd44
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2019-02-22 11:08:41 +0800

    Update v.sh

--- v.sh
+++ v.sh
@@ -264,7 +264,7 @@ modify_crontab(){
 	echo -e "${OK} ${GreenBG} 配置每天凌晨自动升级V2ray内核任务 ${Font}"
 	sleep 2
 	#crontab -l >> crontab.txt
-	echo "20 12 * * * bash /root/v2ray/go.sh | tee -a /root/v2ray/update.log" >> crontab.txt
+	echo "20 12 * * * bash /root/v2ray/go.sh | tee -a /root/v2ray/update.log && service v2ray restart" >> crontab.txt
 	echo "12 0 * * * /etc/init.d/nginx stop" >> crontab.txt
 	echo "15 1 * * * /etc/init.d/nginx restart" >> crontab.txt
 	echo "20 1 * * * /sbin/reboot" >> crontab.txt
